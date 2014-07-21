1a7260116f9cc7a22197dbad5b314c0680d3c1c5
David Swearingen <swearingendw@mac.com>
2014-07-21 17:03:26 -0400

    progress

--- assignment_3/rankall.R
+++ assignment_3/rankall.R
@@ -13,22 +13,41 @@ rankall <- function(outcome, num="best") {
       if(!is.element(outcome, c("heart attack", "heart failure", "pneumonia"))) {
             stop("invalid outcome")
       }
-      
-      unique_states <- unique(d["state"])
-      for(s in unique_states[,1]) {
+      dftmp <- unique(d["state"])
+      unique_states <- sort(dftmp[['state']])
+      final <- data.frame()
+      for(s in unique_states) {
             filtered <- d[d$state == s, ]  # the dataframe rows for this state
-            n <- cbind(filtered, filtered[outcome])  # append a copy of the outcome column of interest
-            n <- n[c(1,2,6)]  # now remove the original outcome columns
-            out <- n[order(n[[3]], n[[1]], na.last=NA), ]
-            if (num == "best") {
-                  print(unlist(out[1,1]))
-            } else if(num == "worst") {
-                  print(unlist(out[nrow(out),1]))
+            if(num > nrow(filtered)) {  # if the row requested is larger than the number of hospitals in the state
+                  final <- rbind(final, data.frame(hospital = NA, state = s))                  
             } else {
-                  print(unlist(out[as.integer(num),1]))  # grab the nth row, where n = the 'num' argument
+                  n <- cbind(filtered, filtered[outcome])  # append a copy of the outcome column of interest
+                  n <- n[c(1,2,6)]  # now remove the original outcome columns
+                  out <- n[order(n[[3]], n[[1]], na.last=NA), ]   # sorting by two columns
+                  if (num == "best") {
+                        val <- unlist(out[1,1])
+                        # print(val)
+                        if(!is.na(val)) {
+                              res <- data.frame(hospital = val, state = s)
+                              final <- rbind(final, res)
+                        }
+                  } else if(num == "worst") {
+                        print("worst")
+                        val <- unlist(out[nrow(out), 1])
+                        print(num)
+                        if(!is.na(val)) {
+                              res <- data.frame(hospital = val, state = s)
+                              final <- rbind(final, res)
+                        }     
+                  } else {
+                        val <- unlist(out[as.integer(num), 1])  # grab the nth row, where n = the 'num' argument
+                        if(!is.na(val)) {
+                              # print(val)
+                              res <- data.frame(hospital = val, state = s)
+                              final <- rbind(final, res)
+                        }
+                  }
             }
       }
-      
-
-
+      # return(final)
 }
